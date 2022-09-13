bbadb3ae9b83bd52f549db3e2296987925c818a6
PedroViniciusMelo <pedro.vinicius.melo.silva@gmail.com>
2022-09-13 15:27:37 -0300

    Adicionado models do projeto

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import {migrate, seed} from "./src/model/migrations";
 
 export default function App() {
+  useEffect( () => {
+    async function initTables () {
+      await migrate()
+      await seed()
+    }
+
+    initTables()
+  }, [])
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
